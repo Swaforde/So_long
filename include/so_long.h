cb00d14f13ca4a87b99de40642ca25a82f80badb
Theo Bouverat <tbouvera@student.42lausanne.ch>
2022-11-14 11:52:55 +0100

    push

--- include/so_long.h
+++ include/so_long.h
@@ -38,5 +38,7 @@ int		ft_get_height(void);
 int		ft_get_width(void);
 void	map_parsing(char **tab, t_map map);
 void	display_wall(char **map, void *mlx, void *mlx_win, t_map data);
+int map_checker(char **map, t_map info);
+
 
 #endif
