baad4c4e13b7f9d357cb2d5a860307b54f75c881
Théo Bouverat <theo@MacBook-Pro-de-Theo.local>
2022-11-14 18:18:08 +0100

    Character checker

--- src/map_format.c
+++ src/map_format.c
@@ -1,5 +1,26 @@
 #include "../include/so_long.h"
 
+int	char_checker(char **map, t_map info)
+{
+	int	i;
+	int	f;
+
+	i = 0;
+	f = 0;
+	while (i < info.height)
+	{
+		while (f < info.width)
+		{
+			if (map[i][f] != 'P' && map[i][f] != 'E' && map[i][f] != '1' && map[i][f] != '0')
+				return (0);
+			f ++;
+		}
+		f = 0;
+		i ++;
+	}
+	return (1);
+}
+
 int	doublon_checker(char **map, t_map info, char c)
 {
 	int	i;
@@ -68,6 +89,11 @@ int	map_format_checker(char **map, t_map info)
 	if (doublon_checker(map, info, 'E') < 1)
 	{
 		ft_printf("La map ne contient aucune sortie !");
+		return (0);
+	}
+	if (char_checker(map, info) != 1)
+	{
+		ft_printf("La map contient des charactère interdit !");
 		return (0);
 	}
 	return (1);
